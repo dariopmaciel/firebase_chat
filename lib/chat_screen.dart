e9c3f0a3d6ff57de9bc25d12377fd94d7b15a68a
Dario de Paula MAciel <116087297+dariopmaciel@users.noreply.github.com>
2023-04-06 14:20:19 -0300

    salvando img + txt

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -17,6 +17,7 @@ class _ChatScreenState extends State<ChatScreen> {
     if (imgFile != null) {
       UploadTask task = FirebaseStorage.instance
           .ref()
+          .child("casa")
           .child(DateTime.now().millisecondsSinceEpoch.toString())
           .putFile(imgFile);
 
